27d8ec98083de5cd22055cba81f04c6b5e71d4ab
gaberani <khs0783@naver.com>
2021-08-02 00:07:48 +0900

    Add: use className

--- game2048/mine/src/component/AboveGame.js
+++ game2048/mine/src/component/AboveGame.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 export default function AboveGame() {
   return (
-    <div className="heading">
-      <div className="title"></div>
-      <div className="scores-container">
-        <div className="score-container"></div>
-        <div className="best-container"></div>
-      </div>
+    <div className="above-game">
+      <p className="game-intro">
+        Join the numbers and get to the <strong>2048 tile!</strong>
+      </p>
+      <a href="/" className="restart-button">
+        New Game
+      </a>
     </div>
   );
 }
