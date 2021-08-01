43fad97e1daa8da3b280e7e3ad4fd0ccbbb88271
gaberani <khs0783@naver.com>
2021-08-01 23:59:17 +0900

    Init: base component

---
+++ game2048/mine/src/component/AboveGame.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function AboveGame() {
+  return (
+    <div className="heading">
+      <div className="title"></div>
+      <div className="scores-container">
+        <div className="score-container"></div>
+        <div className="best-container"></div>
+      </div>
+    </div>
+  );
+}
---
+++ game2048/mine/src/component/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="heading">
+      <div className="title"></div>
+      <div className="scores-container">
+        <div className="score-container"></div>
+        <div className="best-container"></div>
+      </div>
+    </div>
+  );
+}
