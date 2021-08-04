b709763823c1bc60b98bef654e3542bc8befa45d
gaberani <khs0783@naver.com>
2021-08-04 23:55:58 +0900

    Add: use basic components

--- game2048/mine/src/App.js
+++ game2048/mine/src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Header from './component/Header';
+import AboveGame from './component/AboveGame';
+import Game from './component/Game';
 
-function App() {
-  return <div className="container"></div>;
+export default function App() {
+  return (
+    <div className="container">
+      <Header />
+      <AboveGame />
+      <Game />
+    </div>
+  );
 }
