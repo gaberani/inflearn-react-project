c0b586514e035487a9cb55157e1d1b3507eeb6ba
gaberani <khs0783@naver.com>
2021-08-07 23:47:57 +0900

    Add: move tile ftn draft by 4 directions

--- game2048/mine/src/hook/useMoveTile.js
+++ game2048/mine/src/hook/useMoveTile.js
@@ -1,11 +1,26 @@
 import { useEffect } from "react";
 import { addKeyObserver, removeKeyObserver } from "../util/keyboard";
+import { makeTile } from "../util/tile";
 
-export default function useMoveTile() {
-  function moveUp() {}
-  function moveDown() {}
-  function moveLeft() {}
-  function moveRight() {}
+export default function useMoveTile({ tileList, setTileList }) {
+  function moveAndAdd({ x, y }) {
+    const newTileList = moveTile({ tileList, x, y })
+    const newTile = makeTile(newTileList);
+    newTileList.push(newTile)
+    setTileList(newTileList)
+  }
+  function moveUp() {
+    moveAndAdd({ x: 0, y: -1 });
+  }
+  function moveDown() {
+    moveAndAdd({ x: 0, y: 1 });
+  }
+  function moveLeft() {
+    moveAndAdd({ x: -1, y: 0 });
+  }
+  function moveRight() {
+    moveAndAdd({ x: 1, y: 0 });
+  }
   useEffect(() => {
     addKeyObserver('up', moveUp)
     addKeyObserver('down', moveDown)
