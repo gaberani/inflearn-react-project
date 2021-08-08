3cc1f2b2cc4f54f2575636b30bf3e97ac95b2be6
gaberani <khs0783@naver.com>
2021-08-09 00:10:31 +0900

    Add: header score variable

--- game2048/mine/src/component/Header.js
+++ game2048/mine/src/component/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function Header() {
+export default function Header({ score, bestScore}) {
   return (
     <div className="heading">
       <div className="title">2048</div>
       <div className="scores-container">
-        <div className="score-container">362</div>
-        <div className="best-container">438</div>
+        <div className="score-container">{score}</div>
+        <div className="best-container">{bestScore}</div>
       </div>
     </div>
   );
