bbe536bdae8c5dc2715396507242ccf285f3facb
gaberani <khs0783@naver.com>
2021-08-08 23:56:11 +0900

    Add: header title, sample score

--- game2048/mine/src/component/Header.js
+++ game2048/mine/src/component/Header.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Header() {
   return (
     <div className="heading">
-      <div className="title"></div>
+      <div className="title">2048</div>
       <div className="scores-container">
-        <div className="score-container"></div>
-        <div className="best-container"></div>
+        <div className="score-container">362</div>
+        <div className="best-container">438</div>
       </div>
     </div>
   );
